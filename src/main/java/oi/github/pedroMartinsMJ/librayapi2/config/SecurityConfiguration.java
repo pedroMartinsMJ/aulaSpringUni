0ed5c045ad46d8072ba770aa1b1a2173a907a908
pedroMartinsMJ <pedrohmrs2004@gmail.com>
2025-08-06 16:51:41 -0300

    intrudução ao ao spring security

--- src/main/java/oi/github/pedroMartinsMJ/librayapi2/config/SecurityConfiguration.java
+++ src/main/java/oi/github/pedroMartinsMJ/librayapi2/config/SecurityConfiguration.java
@@ -2,10 +2,16 @@ package oi.github.pedroMartinsMJ.librayapi2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +22,40 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults()) //padrão
+//                .httpBasic(Customizer.withDefaults()) //padrão
+                .formLogin(confugurer ->{
+                    confugurer.loginPage("/login");
+                })
                 .authorizeHttpRequests( authorize -> {
-                    authorize.anyRequest().authenticated();
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers("/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+
+                    authorize.anyRequest().authenticated();// anyResquest tem que ser a aultima
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
--- src/main/java/oi/github/pedroMartinsMJ/librayapi2/controles/LoginViewController.java
+++ src/main/java/oi/github/pedroMartinsMJ/librayapi2/controles/LoginViewController.java
@@ -1,7 +1,13 @@
 package oi.github.pedroMartinsMJ.librayapi2.controles;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
 }
